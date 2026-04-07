1199a22c721c30c5d73e2ccd03bafe6adde2479c
Ladzuu <lucaslabiouse1243@gmail.com>
2026-04-07 09:58:36 +0200

    Problems Solved : Dist -> Online

--- src/Experience/sources.js
+++ src/Experience/sources.js
@@ -4,12 +4,12 @@ export default [
         type: 'cubeTexture',
         path: 
         [
-            '/textures/environmentMap/px.jpg',
-            '/textures/environmentMap/nx.jpg',
-            '/textures/environmentMap/py.jpg',
-            '/textures/environmentMap/ny.jpg',
-            '/textures/environmentMap/pz.jpg',
-            '/textures/environmentMap/nz.jpg'
+            'textures/environmentMap/px.jpg',
+            'textures/environmentMap/nx.jpg',
+            'textures/environmentMap/py.jpg',
+            'textures/environmentMap/ny.jpg',
+            'textures/environmentMap/pz.jpg',
+            'textures/environmentMap/nz.jpg'
         ]
     },
     {
