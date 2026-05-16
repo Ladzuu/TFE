d5de498af6861fb0c6a44a6fdce47dfc5e945ca4
Ladzuu <lucaslabiouse1243@gmail.com>
2026-05-16 16:13:09 +0200

    Dist Resolved -> Multiple HTML Pages

--- src/script.js
+++ src/script.js
@@ -1,5 +1,18 @@
 import './style.scss'
+import EventEmitter from './Experience/Utils/EventEmitter.js'
+import Ressources from './Experience/Utils/Resources.js'
+import Sizes from './Experience/Utils/Sizes.js'
+import Time from './Experience/Utils/Time.js'
+import Environment from './Experience/World/Environment.js'
+import Island from './Experience/World/Island.js'
+import Map from './Experience/World/Map.js'
+import Temple from './Experience/World/Temple.js'
+import Village from './Experience/World/Village.js'
+import World from './Experience/World/World.js'
+import Camera from './Experience/Camera.js'
 import Experience from './Experience/Experience.js'
+import Renderer from './Experience/Renderer.js'
+import sources from './Experience/sources.js'
 
 const canvas = document.querySelector('canvas')
 const experience = new Experience(canvas)
--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,6 @@
+import { resolve } from 'path'
 import restart from 'vite-plugin-restart'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default {
     base: './', // Base public path when served in production
@@ -13,10 +15,26 @@ export default {
     {
         outDir: '../dist', // Output in the dist/ folder
         emptyOutDir: true, // Empty the folder first
-        sourcemap: true // Add sourcemap
+        sourcemap: true, // Add sourcemap
+        rollupOptions: // Add multiple HTML pages
+        {
+            input:
+            {
+                main: resolve(__dirname, 'src/index.html'),
+                map: resolve(__dirname, 'src/map.html'),
+                temple: resolve(__dirname, 'src/temple.html'),
+                island: resolve(__dirname, 'src/island.html'),
+                village: resolve(__dirname, 'src/village.html')
+            }
+        }
     },
     plugins:
     [
-        restart({ restart: [ '../static/**', ] }) // Restart server on static file change
+        restart({ restart: [ '../static/**', ] }), // Restart server on static file change
+        // viteStaticCopy({
+        //     targets: [
+        //         { src: '../src/**/*', dest: '' }
+        //     ]
+        // })
     ],
 }
